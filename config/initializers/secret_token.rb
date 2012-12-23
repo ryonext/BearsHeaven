833a175bb8e971c48e4e37b0c44ca6adca4b2a1e
ryonext <ryonext.s@gmail.com>
2012-12-23 20:24:47 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BearsHeavenRails::Application.config.secret_token = '29579a915c47bbbd825803d2f9c46889fd2f7c92abc738014516e2850c3c694ac1cd559700ecf68ae655ab02844386c3eccdb41ec4feb61b833f4033a2bc9518'
