c2b12a22bd2276c7ba22616ccef0610b9674bfdd
ryonext <ryonext.s@gmail.com>
2013-05-12 15:03:03 +0900

    add sample initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Score.create(name: "nanika", point:10, magnification: 10, difficulty: "Very Hard")
